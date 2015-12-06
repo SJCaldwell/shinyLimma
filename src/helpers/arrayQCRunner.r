1e24d63602dd0beba492bc875c57d9eb29634e56
SJCaldwell <shane.caldwell12@ncf.edu>
2015-12-06 02:15:06 -0500

    fixed paths

--- src/helpers/arrayQCRunner.r
+++
@@ -1,9 +0,0 @@
-#library(arrayQualityMetrics)
-#library(lumi)
-#setwd("C://Users//jimc//Desktop//ShinyLimmaWorkspace//data_shinyLimma")
-
-QC_Reporter <- function(path){
-  #x.lumi <- lumiR.batch(path)
-  #arrayQualityMetrics(x.lumi, outdir = "QCReport")
-  cat("Done and done.")
-}
