fa46515f8534958215c03bb1ccb0dc7db89fb5ac
SJCaldwell <shane.caldwell12@ncf.edu>
2016-09-19 05:27:09 -0400

    keeping repo clean

--- helpers/helper.R
+++ helpers/helper.R
@@ -1,12 +1,10 @@
-
-
-getExploratoryPlot <- function(input, x){
+getExploratoryPlot <- function(input, x, targets){
 
 	if(input == 1){
 		return (NULL)
 	}
 	else if (input == 2){
-        heatmap <- heatmapBuilder(x)
+        heatmap <- heatmapBuilder(x, targets)
         return (heatmap)
 	}
 	else if (input == 3){
