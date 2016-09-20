d46bf241a5a2259cd94491383e64b8e1fbbb4b2e
SJCaldwell <shane.caldwell12@ncf.edu>
2016-09-20 07:44:31 -0400

    bug that has matches highlight more than the groups after loading contrast matrix...
    it's gotta be in the which() calculation. make sure which does exact matches and not just %in% matching

--- server.R
+++ server.R
@@ -193,24 +193,32 @@ hideModal <- function(id, session) {
   })
    
    validate <- function(mat){
-     cat(input$group1Contrast, " is input$group1Contrast")
-	  group1 <- strsplit(input$group1Contrast, split = ".", fixed = TRUE)
-    group2 <- strsplit(input$group2Contrast, split = ".", fixed = TRUE)
+	  group1 <- strsplit(input$group1Contrast, split = ".", fixed = TRUE)[[1]]
+    group2 <- strsplit(input$group2Contrast, split = ".", fixed = TRUE)[[1]]
+    group1_attributes = list()
+    group2_attributes = list()
     for (i in 1:ncol(userInput$targetManager$targets)){
 		updateTableStyle(session, "targetsTable", "valid",
                   which(cachedTargets[[i]] != ""), i)
-}  
+    }  
     for (i in 1:ncol(userInput$targetManager$targets)){
-    for (j in 1:length(group1[[1]])){
+    cat("length is... ", length(group2))
+    cat(group2)
+    for (j in 1:length(group1)){
+      group1_attributes[[length(group1_attributes) + 1]] = which(cachedTargets[[i]] != group1[j])
+    }
+    for (j in 1:length(group2)){
+      group2_attributes[[length(group2_attributes) + 1]] = which(cachedTargets[[i]] != group2[j])
+    }
+    }
+    for (i in 1:length(group1)){
     updateTableStyle(session, "targetsTable", "warning", 
-                  which(cachedTargets[[i]] == group1[[1]][j]), i)
-		}
-    for (j in 1:length(group2[[1]])){
-	updateTableStyle(session, "targetsTable", "invalid", 
-				  which(cachedTargets[[i]] == group2[[1]][j]), i)
-
-}
-}
+                     Reduce(intersect, group1_attributes), i)
+    }
+    for (i in 1:length(group2)){
+    updateTableStyle(session, "targetsTable", "invalid",
+                     Reduce(intersect, group2_attributes), i)
+    }
 	}
 
    initialize <- function(mat){
